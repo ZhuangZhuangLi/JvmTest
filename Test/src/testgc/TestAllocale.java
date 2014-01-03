9aa509f316f6dc01ebceef61777525c7e3ed958f
ZhuangZhuangLi <15123130007@126.com>
2014-01-03 11:57:25 +0800

    ttt

---
+++ Test/src/testgc/TestAllocale.java
@@ -0,0 +1,15 @@
+package testgc;
+
+public class TestAllocale {
+	
+	public final static int _1MB=1024*1024;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte[] _all1=new byte [2*_1MB];
+		byte[] _all2=new byte [2*_1MB];
+		byte[] _all3=new byte [2*_1MB];
+		byte[] _all4=new byte [6*_1MB];
+	}
+
+}
